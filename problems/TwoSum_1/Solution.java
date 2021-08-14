d4edfe716bfc765008d7bfe1c9d9f81818ddc328
”Junior” <rip05-04-1990@yandex.ru>
2021-08-14 13:49:56 +0400

    two sum

---
+++ problems/TwoSum_1/Solution.java
@@ -0,0 +1,22 @@
+package problems.TwoSum_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            int res = target - nums[i];
+            if(map.containsKey(res) && map.get(res) != i) {
+                return new int[] {i, map.get(res)};
+            }
+        }
+        throw new IllegalArgumentException("exception");
+    }
+}
