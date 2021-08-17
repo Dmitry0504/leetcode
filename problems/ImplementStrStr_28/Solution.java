9ec149d848a46b3b7c1c84257b36bbc8c5dbab92
”Junior” <rip05-04-1990@yandex.ru>
2021-08-17 23:39:06 +0400

    implement strStr()

---
+++ problems/ImplementStrStr_28/Solution.java
@@ -0,0 +1,7 @@
+package problems.ImplementStrStr_28;
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
--- problems/Solution1.java
+++ problems/Solution1.java
@@ -3,63 +3,27 @@ package problems;
 public class Solution1 {
 
     public static void main(String[] args) {
-        ListNode first = new ListNode(1,
-                new ListNode(2,
-                        new ListNode(3,
-                                new ListNode(4))));
-
+//        ListNode first = new ListNode(1,
+//                new ListNode(2,
+//                        new ListNode(3,
+//                                new ListNode(4))));
+//
 //        ListNode listNode = new ListNode();
 //        new Solution1().removeNthFromEnd(listNode, 1);
 
-        ListNode node = new Solution1().removeNthFromEnd(first, 4);
-        while (node != null) {
-            System.out.println(node.val);
-            node = node.next;
-        }
+//        ListNode node = new Solution1().removeNthFromEnd(first, 4);
+//        while (node != null) {
+//            System.out.println(node.val);
+//            node = node.next;
+//        }
+        System.out.println(new Solution1().strStr("", ""));
+
 
     }
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        int currIndex = 1;
-        int size = 0;
-        ListNode currentNode = head;
-        ListNode newHead = currentNode;
-        ListNode temp = head;
-        while (temp != null) {
-            temp = temp.next;
-            size++;
-        }
-
-        int indexElementForDelete = size - n + 1;
-
-        if(indexElementForDelete == 1) {
-            return head.next;
-        }
-        while (currIndex < indexElementForDelete) {
-
-            if(currIndex == indexElementForDelete - 1) {
-                currentNode.next = currentNode.next.next;
-            }
-            currentNode = currentNode.next;
-            currIndex++;
-        }
-        return newHead;
-
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
     }
+
+
 }
- class ListNode {
-     int val;
-     ListNode next;
-
-     ListNode() {
-     }
-
-     ListNode(int val) {
-         this.val = val;
-     }
-
-     ListNode(int val, ListNode next) {
-         this.val = val;
-         this.next = next;
-     }
- }
