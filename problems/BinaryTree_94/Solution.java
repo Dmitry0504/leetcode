2889195e37ebd7fd086bba251ba63b2efc256c98
”Junior” <rip05-04-1990@yandex.ru>
2021-08-26 22:20:02 +0300

    binary tree inorder traversal, problem 94

---
+++ problems/BinaryTree_94/Solution.java
@@ -0,0 +1,54 @@
+package problems.BinaryTree_94;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, null,
+                new TreeNode(2,
+                        new TreeNode(3), null)
+        );
+
+        System.out.println(new Solution().inorderTraversal(root));
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> integers = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+
+        TreeNode currentNode = root;
+
+        while (currentNode != null || !stack.isEmpty()) {
+            while (currentNode != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.left;
+            }
+            currentNode = stack.pop();
+            integers.add(currentNode.val);
+            currentNode = currentNode.right;
+        }
+
+        return integers;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
