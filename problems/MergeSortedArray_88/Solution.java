b16759f06b6de11bef183c6fc3d7dd2da848b0e3
”Junior” <rip05-04-1990@yandex.ru>
2021-10-09 16:00:35 +0300

    problem 88, merge sorted array

--- problems/MergeSortedArray_88/Solution.java
+++ problems/MergeSortedArray_88/Solution.java
@@ -1,13 +1,21 @@
 package problems.MergeSortedArray_88;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
+        int[] firstArray = {1, 4, 0, 0};
+        int[] secondArray = {2, 7};
 
+        new Solution().merge(firstArray, 2, secondArray, 2);
+        System.out.println(Arrays.toString(firstArray));
     }
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = 0; i < nums1.length; i++) {
-            
+        for (int i = 0; i < n; i++) {
+            nums1[m++] = nums2[i];
         }
+        Arrays.sort(nums1);
+
     }
 }
