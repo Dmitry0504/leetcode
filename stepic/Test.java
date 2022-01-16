0d068467866a7a4f1ed95980a419bceca09b69ae
”Junior” <rip05-04-1990@yandex.ru>
2022-01-16 16:26:22 +0300

    Quiz

--- stepic/Test.java
+++ stepic/Test.java
@@ -2,8 +2,11 @@ package stepic;
 
 public class Test {
     public static void main(String[] args) {
-        Object b = new Object();
-        A a = (A) b;
+//        Object b = new Object();
+//        A a = (A) b;
+
+        System.out.println(true ^ false);
+        System.out.println(true != false);
     }
 }
 
